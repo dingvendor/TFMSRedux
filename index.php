fc257921008fb720beecedd54656ef76ac2f2db3
Dan Taylor <dan@dan-AOA110.(none)>
2010-11-22 17:26:39 +0000

    Initial Commit of project

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/***
+ * TFMS Redux
+ * This may never see the light of day.
+ * The purpose of this project is to let me blow of steam whilst making something which
+ * may be of use to the outside world. 
+ *
+ * Feature suggestions are welcome, but they may be ignored.
+ *
+ * @author Dan Taylor
+ */
+?>
+Hello TFMS Redux
